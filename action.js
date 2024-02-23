590c94af13cf0976e2da325b6b32e0bbd665aac9
Francis Louie Antonio <francis.antonio0927@gmail.com>
2024-02-23 22:05:40 +0800

    Completed with Images

--- action.js
+++ action.js
@@ -1,7 +1,10 @@
 const bottomNavbar = document.querySelector(".bottomNavbar")
 const buttonMore = document.querySelector("#more")
 const fixedNavbar = document.querySelector(".stickyHeader")
-const container = document.getElementById("container")
+const ytVideo = document.getElementById("youtubeVideo")
+const videoImageBg = document.querySelector(".imageVideoBg")
+const videoPlayButton = document.querySelector(".circle")
+
 let option = false;
 let lastScroll = window.scrollY;
 
@@ -12,20 +15,25 @@ const OpenNav = () => {
 }
 
 document.addEventListener("scroll", ()=>{
-    // fixedNavbar.style.top =  window.scrollY <= 70? "unset" : "0"
- 
+    let up = option? 430: 70
+    let down = option? 450: 100
 
-    if((window.scrollY > lastScroll) && window.scrollY > 100){
+    if((window.scrollY > lastScroll) && window.scrollY > down){
         fixedNavbar.style.top = "-75px"
     }else{
-        fixedNavbar.style.position = window.scrollY <= 70? "absolute" : "fixed"
-        fixedNavbar.style.top = window.scrollY <= 70? "unset" : "0"
-
+        fixedNavbar.style.position = window.scrollY <= up? "absolute" : "fixed"
+        fixedNavbar.style.top = window.scrollY <= up? "unset" : "0"
     }
 
     lastScroll = window.scrollY
+})  
 
-    console.log(window.scrollY)
-})   
+const playVideo = () => {
+    videoImageBg.style.display = "none"
+    videoPlayButton.style.display = "none"
+    ytVideo.style.display = "block"
+
+    ytVideo.src += "?&autoplay=1"
+}
 
 
